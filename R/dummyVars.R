e8d19379bf7002c8614d28ee61abbf16ba82bd37
ludgergoeminne <ludgergoeminne@gmail.com>
2016-11-06 21:18:46 +0100

    Add files via upload

--- R/dummyVars.R
+++ R/dummyVars.R
@@ -213,3 +213,23 @@ predict.dummyVars <- function(object, newdata, na.action = na.pass, ...)
   x[, colnames(x) != "(Intercept)", drop = FALSE]
 }
 
+
+#' @rdname dummyVars
+#' @export
+contr.ltfr <- function (n, contrasts = TRUE, sparse = FALSE)
+{
+  if (is.numeric(n) && length(n) == 1L) {
+    if (n > 1L)
+      levels <- as.character(seq_len(n))
+    else stop("not enough degrees of freedom to define contrasts")
+  }
+  else {
+    levels <- as.character(n)
+    n <- length(n)
+  }
+  contr <- .RDiag(levels, sparse = sparse)
+  if (contrasts) {
+    if (n < 2L) stop(gettextf("contrasts not defined for %d degrees of freedom", n - 1L), domain = NA)
+  }
+  contr
+}
