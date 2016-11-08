f608eebde360b714bd5fd45f6b0e63adde692b3d
ludgergoeminne <ludgergoeminne@gmail.com>
2016-11-08 11:00:17 +0100

    Add files via upload

--- R/MSnSet2protdata.R
+++ R/MSnSet2protdata.R
@@ -53,7 +53,7 @@ MSnSet2protdata <- function(MSnSet, accession, annotations=NULL, quant_name="qua
 
     sel <- fData[,accession] == accessions[i]
     intensities <- exprs[sel,,drop=FALSE]
-    properties <- fData[sel,]
+    properties <- fData[sel,,drop=FALSE]
     #If for the same accession in an annation column, there would be multiple different values, just paste them together.
     annotation_matrix[i,] <- apply(properties[,annotations], 2, function(x){paste0(unique(x))})
     properties <- properties[,-which(colnames(properties) %in% c(colnames(properties[,accession,drop=FALSE]),colnames(properties[,annotations,drop=FALSE]))), drop=FALSE]
