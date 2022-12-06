321698abb000b07e38fad83706259e360a7d4e84
Ludger Goeminne <ludgergoeminne@gmail.com>
2022-12-06 17:53:04 +0100

    Update server.R
    Added support for mzTab headers where the header is not the first line in the file.

--- inst/App-MSqRob/server.R
+++ inst/App-MSqRob/server.R
@@ -156,7 +156,18 @@ shinyServer(function(input, output, session) {
         a <- gsub("^\\\"","",test)
         b <- gsub("\\\"$","",a)
         make.names(b)
-      } else{
+     } else if(input$input_type=="mzTab"){
+      
+        tmp.lines <- readLines(con=peptidesDatapath())
+        skip <- (which(grepl("peptide_abundance_study_variable", tmp.lines))-1)[1]
+        colInt <- grep("peptide_abundance_study_variable", strsplit(tmp.lines[(skip+1)], split = sep)[1][[1]])
+      
+        test <- read.table(peptidesDatapath(), sep=",", skip=skip, nrows=1, quote="\\\"", comment.char = "", stringsAsFactors = FALSE)
+        a <- gsub("^\\\"","",test)
+        b <- gsub("\\\"$","",a)
+        make.names(b)
+      
+    } else{
         make.names(as.vector(as.matrix(read.table(peptidesDatapath(), nrows=1, sep="\t", quote="", comment.char = ""))))
       }
     }
